4b79abddb7d82124609bdef1d423b527eb997b02
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-05-04 15:16:09 +0200

    IWA_2.4 Challenge 1 commit

--- Project IWA2/IWA_2.4 Challenge 1/js/scripts.js
+++ Project IWA2/IWA_2.4 Challenge 1/js/scripts.js
@@ -1,6 +1,6 @@
 /* This is the firstname of the user */
 
-const user = 'John'
+const user = 'john'
 
 
 /* This is the lastname of the user */
@@ -8,4 +8,25 @@ const user = 'John'
 const surname = 'Smith'
 
 
-console.log(user ,surname)
+console.log(user,surname)
+
+
+// Questions
+
+
+/* Why did the original code not work?
+
+// Because I was supposed to remove the semicolons and insert a comma between user and surname.
+
+What changes did you need to make to achieve the intended behaviour?
+
+// In the comments i've just corrected the closing comments then in the  console.log i have removed the semicolons then inserted a comma.
+
+Why did the change result in the intended behaviour?
+
+// Is because without these changes the codes would not work that way.
+
+Are there any other ways in which it could have been solved?*/
+
+// Yes, if maybe I can divide the user and surname in the other console.log it will still work.
+
