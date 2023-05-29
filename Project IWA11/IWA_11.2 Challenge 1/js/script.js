9f4009cd18c27507167917e9bbeb82f74433c6d0
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-05-29 14:03:46 +0200

    IWA_11: Challenge 1 commit

---
+++ Project IWA11/IWA_11.2 Challenge 1/js/script.js
@@ -0,0 +1,40 @@
+
+// script.js
+
+const root1 = document.querySelectorAll(order1)
+   const biscuits1 = document.querySelector(biscuits),
+   const donuts1 = document.querySelector(donuts),
+   const pancakes1 = document.querySelector(pancakes),
+   const status1 =  document.querySelector(status)
+
+   biscuits1.textContent = order1.getAtribute =('Data - biscuits');
+donuts1.textContent = order1.getAtribute =('Data - donuts');
+pancakes1.textContent = order1.getAtribute =('Data - pancakes');
+status1.textContent = order1.getAtribute =('Data - delivered ') === 'true' ? 'Delivered';
+
+
+const root2 = document.querySelectorAll(order2),
+const biscuits2 = document.querySelector(biscuits),
+  const donuts2 =  document.querySelector(donuts),
+    const pancakes2 = document.querySelector(pancakes),
+    const status2 = document.querySelector(status)
+
+
+    biscuits2.textContent = order2.getAtribute =('Data - biscuits');
+donuts2.textContent = order2.getAtribute =('Data - donuts');
+pancakes2.textContent = order2.getAtribute =('Data - pancakes');
+status2.textContent = order2.getAtribute =('Data - delivered ') === 'true' ? 'Delivered';
+
+
+const root3 = document.querySelector(order3)
+  const biscuits3 =  document.querySelector(biscuits),
+   const donuts3 = document.querySelector(donuts),
+   const pancakes3 = document.querySelector(pancakes),
+  const status3 = document.querySelector(status)
+
+
+biscuits3.textContent = order3.getAtribute =('Data - biscuits');
+donuts3.textContent = order3.getAtribute =('Data - donuts');
+pancakes3.textContent = order3.getAtribute =('Data - pancakes');
+status3.textContent = order3.getAtribute =('Data - delivered ') === 'false' ? 'Delivered';
+
