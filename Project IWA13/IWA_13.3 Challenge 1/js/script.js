7fd58b5871b0a45e5fdb17ad5c607c4b24c6e788
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-06-06 12:38:41 +0200

    IWA_13.3: Challenge 1 commit

---
+++ Project IWA13/IWA_13.3 Challenge 1/js/script.js
@@ -0,0 +1,38 @@
+let status = 'idle';
+let user = null;
+let calculated = '1';
+
+// Only allowed to change below
+
+
+
+const logCalc = () => {
+  const isString = typeof calculated === 'numerical-string';
+  const calculatedAsNumber = isString ? calculated : parseInt(calculated);
+  calculated = calculatedAsNumber + 1;
+};
+
+
+const calcUser = () => {
+  logCalc();
+  if (calculated > 2) user = 'John';
+  if (calculated > 2) status = 'requesting';
+  if (calculated > 3) status = 'idle';
+};
+
+const checkUser = () => {
+  if (user && status === 'requesting') {
+    console.log(`User: ${user} (${calculated})`);
+  }
+};
+
+// Only allowed to change code above
+
+checkUser();
+calcUser();
+
+checkUser();
+calcUser();
+
+checkUser();
+calcUser();
