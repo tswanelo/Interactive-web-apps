081a9b23c4f6817554f4d494729d53c4c4780a70
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-06-06 21:30:33 +0200

    IWA_8.2 Challenge 1

--- Project IWA8/IWA_8.2 Challenge 1/js/script.js
+++ Project IWA8/IWA_8.2 Challenge 1/js/script.js
@@ -1,3 +1,4 @@
+
 const leoName = 'Leo Musvaire'
 const leoNumber = '2'
 const leoStreet = 'Church St.'
@@ -17,31 +18,30 @@ const leo = {
 	leoname: leoName,
 	leoBalance: parseInt (leoBalance),
 	accessId: '47afb389-8014-4d0b-aff3-e40203d2107f',
-	age: 24,},
+	age: 24,
 
-	address = {
+	address :{
 		leoNumber: parseInt (leoNumber),
 		leoStreet: leoStreet,
 		postalCode: parseInt (leoPostal),
 	}
+}
 
-console.log(leo.leoname,leo.leoBalance,leo.accessId,leo.age,address.leoNumber,address.leoStreet,address.postalCode)
+console.log(leo.leoname,leo.age,leo.accessId,leo.leoBalance,leo['address'])
+
 
 const sarah = {
 	name: sarahName + sarahSurname,
 	age : 62,
 	AccessId: '6b279ae5-5657-4240-80e9-23f6b635f7a8',
-	balance : parseInt(sarahBalance)},
+	balance : parseInt(sarahBalance),
 
-	Address = {
+ Address :{
 		number : parseInt(sarahNumber),
 		street : sarahStreet,
 		postalCode : parseInt(sarahPostal),
 	}	
+}
 
-
-console.log(sarah.name,sarah.age,sarah.AccessId,sarah.balance,Address.number,Address.street,Address.postalCode)
-
-
-
+console.log(sarah.name,sarah.age,sarah.AccessId,sarah.balance, sarah['Address'])
 
