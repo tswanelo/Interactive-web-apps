d3a376c55b10dd93235bf388e3768e12426506fc
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-06-08 15:43:27 +0200

    IWA_14: Challenge 1 & 2 commit

---
+++ Project IWA14/IWA_14.2 Challenge 1/js/script.js
@@ -0,0 +1,19 @@
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
+
+const logTwice = (parameter) => {
+  console.log(parameter)
+  console.log(parameter)
+}
+
+function Codehobby () {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+}
+
+Codehobby()
+
+
+
+
+
