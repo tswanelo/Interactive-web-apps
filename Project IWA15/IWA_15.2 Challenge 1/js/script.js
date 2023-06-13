f3aa37fe85267737423305cee5f35d2e0e3e001f
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-06-13 12:54:15 +0200

    IWA_15.2 Challenge 1 commit

---
+++ Project IWA15/IWA_15.2 Challenge 1/js/script.js
@@ -0,0 +1,53 @@
+// scripts.js
+
+const data = {
+	lists: [
+		['first', [15, 11, 13, 7, 5]],
+		['second', [2, 6, 8, 4, 14, 12, 10]],
+		['third', [9, 3, 1]],
+	]
+}
+
+// Only edit below
+const first = data.lists[0][1]
+const second = data.lists[1][1] 
+const third = data.lists[2][1] 
+
+const result = [].concat(first,second,third)
+
+
+const extractBiggest = () => {
+	if (first > second) {
+		return first
+	}
+
+	if (third < 1) {
+		return second
+	}
+	
+	return third
+}
+
+
+// Only edit above
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+console.log(result)
+  
