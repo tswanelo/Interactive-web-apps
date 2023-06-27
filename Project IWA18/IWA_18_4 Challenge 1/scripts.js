5642bcc6f7540f7d8436cdf8d8f113a144142aa2
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-06-27 13:19:19 +0200

    IWA_18 Challenge 1

---
+++ Project IWA18/IWA_18_4 Challenge 1/scripts.js
@@ -0,0 +1,85 @@
+document.querySelector('[data-add]')
+document.querySelector('[data-help]').addEventListener('click', () => {
+document.querySelector('[data-help-overlay]').showModal();
+});
+
+document.querySelector('[data-help-cancel]').addEventListener('click', () => {
+    document.querySelector('[data-help-overlay]').close();
+});
+
+document.querySelector('[data-help-cancel]').addEventListener('click', () => {
+    document.querySelector('[data-help-overlay]').close();
+    document.querySelector('[data-add]').focus();
+});
+
+document.querySelector('[data-add]').addEventListener('click', () => {
+    document.querySelector('[data-add-overlay]').showModal();
+});
+
+document.querySelector('[data-add-cancel]').addEventListener('click', () => {
+    document.querySelector('[data-add-overlay]').close();
+})
+
+
+
+
+
+/**
+ * A handler that fires when a user drags over any element inside a column. In
+ * order to determine which column the user is dragging over the entire event
+ * bubble path is checked with `event.path` (or `event.composedPath()` for
+ * browsers that don't support `event.path`). The bubbling path is looped over
+ * until an element with a `data-area` attribute is found. Once found both the
+ * active dragging column is set in the `state` object in "data.js" and the HTML
+ * is updated to reflect the new column.
+ *
+ * @param {Event} event 
+ */
+const handleDragOver = (event) => {
+    event.preventDefault();
+    const path = event.path || event.composedPath()
+    let column = null
+
+    for (const element of path) {
+        const { area } = element.dataset
+        if (area) {
+            column = area
+            break;
+        }
+    }
+
+    if (!column) return
+    updateDragging({ over: column })
+    updateDraggingHtml({ over: column })
+}
+
+
+const handleDragStart = (event) => {}
+const handleDragEnd = (event) => {}
+const handleHelpToggle = (event) => {}
+const handleAddToggle = (event) => {}
+const handleAddSubmit = (event) => {}
+const handleEditToggle = (event) => {}
+const handleEditSubmit = (event) => {}
+const handleDelete = (event) => {}
+
+html.add.cancel.addEventListener('click', handleAddToggle)
+html.other.add.addEventListener('click', handleAddToggle)
+html.add.form.addEventListener('submit', handleAddSubmit)
+
+html.other.grid.addEventListener('click', handleEditToggle)
+html.edit.cancel.addEventListener('click', handleEditToggle)
+html.edit.form.addEventListener('submit', handleEditSubmit)
+html.edit.delete.addEventListener('click', handleDelete)
+
+html.help.cancel.addEventListener('click', handleHelpToggle)
+html.other.help.addEventListener('click', handleHelpToggle)
+
+for (const htmlColumn of Object.values(html.columns)) {
+    htmlColumn.addEventListener('dragstart', handleDragStart)
+    htmlColumn.addEventListener('dragend', handleDragEnd)
+}
+
+for (const htmlArea of Object.values(html.area)) {
+    htmlArea.addEventListener('dragover', handleDragOver)
+}
