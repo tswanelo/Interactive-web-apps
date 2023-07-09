e86cd0d49dec6674443a00e0ecc848b0df51e3c9
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-07-09 09:16:17 +0200

    commit

--- Project IWA2/IWA_2.6 Challenge 3/js/scripts.js
+++ Project IWA2/IWA_2.6 Challenge 3/js/scripts.js
@@ -1,14 +1,11 @@
 
 // It is important to show the following message in order to:
-// - to assure users
-// - to scare hackers
-//- to impress investors
+// - assure users
+// - scare hackers
+// - impress investors
 
+console.warn('Security scan starting');
 
-console.warn('Security scan starting')  // I've added a quotation mark.
+/* It is important to let users know when they can close the page */
 
-/* It is important to let user know when they can close the page  */
-
-console.info('Please wait for scan to complete before closing the browser') // I removed the full stop then I added a quotation mark.
-
-
+console.info('Please wait for the scan to complete before closing the browser.');
