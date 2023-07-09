57e13e5251a5be658b6b5461a730b5343c854519
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-07-09 09:16:30 +0200

    commit

--- Project IWA4/IWA_4.3 Challenge 1/js/script.js
+++ Project IWA4/IWA_4.3 Challenge 1/js/script.js
@@ -1,44 +1,38 @@
-const date= 2050
-const status= 'student'
-let count = 0  // I removed the const count then added let count.
+const date = 2050;
+const status = 'student';
+let count = 0;
 
-if (date === 2050) { // I added === equal sign
-	
-	count = (count + 5) // 
+if (date === 2050) {
+  count = count + 5;
 
-	console.log(date,"January", 'New Year’s Day') 
-	console.log(date,"March", 'Human Rights Day')
+  console.log(date, "January", 'New Year’s Day');
+  console.log(date, "March", 'Human Rights Day');
 
-	console.log(date, 'April')
-	console.log(date,'Good friday')
-	console.log(date, 'Family Day')
-	console.log(date, 'Freedom Day')
-	
-	
+  console.log(date, 'April');
+  console.log(date, 'Good Friday');
+  console.log(date, 'Family Day');
+  console.log(date, 'Freedom Day');
 
-     function value (student){
-		let count = (count + 1)
-	  console.log(status,'June', 'Youth Day')
-		
- }
-    count =(count + 3) 
-	console.log(date,'August', 'Women’s Day')
-	console.log(date,'September', 'Heritage Day')
-	console.log(date, 'December')
-	console.log(date, 'Day of Reconciliation')
-	
-
-	function gen (parent) {
-		let count = (count + 1)
-	  console.log(status, 'Christmas Day')
-	
+  function value(student) {
+    count = count + 1;
+    console.log(status, 'June', 'Youth Day');
   }
-     count = (count + 1)
-	console.log(date, 'Day of Goodwill')
-	
 
-	console.log('Your status is:', status)
-console.log('The year is:', date)
-console.log('The total holidays is:', count)
+  count = count + 3;
+  console.log(date, 'August', "Women's Day");
+  console.log(date, 'September', 'Heritage Day');
+  console.log(date, 'December');
+  console.log(date, 'Day of Reconciliation');
 
+  function gen(parent) {
+    count = count + 1;
+    console.log(status, 'Christmas Day');
+  }
+
+  count = count + 1;
+  console.log(date, 'Day of Goodwill');
+
+  console.log('Your status is:', status);
+  console.log('The year is:', date);
+  console.log('The total number of holidays is:', count);
 }
