f10a667c56c9213250af767f615e8f99a3db72ea
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-07-09 09:17:16 +0200

    commit

--- SecondChallenge/js/script.js
+++ SecondChallenge/js/script.js
@@ -1,18 +1,38 @@
-const WeekDays = 7
-const RentPerDayCost = '$40'
-const OfferSevenMore = '$50'
-const OfferThreeMore = '$20'
+const SevenDays = 7
+const RentPerDayCost = 40
+const OfferSevenMore = 50
+const OfferThreeMore = 20
 
 
 
 
-function CostForRental(WeekDays) {
-    console.log(WeekDays * RentPerDayCost - (WeekDays >= 7 ? OfferSevenMore : WeekDays >= 3 ? OfferThreeMore : 0)) ;
+function CostForRental(SevenDays) {
+console.log(SevenDays * RentPerDayCost - (SevenDays >= 7 ? OfferSevenMore : SevenDays >= 3 ? OfferThreeMore : 0)) ;
   }
 
 
 
 CostForRental(1)
 CostForRental(2)
-CostForRental(3)
-CostForRental(4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
