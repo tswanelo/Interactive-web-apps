a7c94a886e9f5c776b1b23e31a6ffe6e9b18dc7a
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-07-09 09:17:01 +0200

    commit

--- Project IWA7/IWA_7.4 Challenge 2/js/script.js
+++ Project IWA7/IWA_7.4 Challenge 2/js/script.js
@@ -1,18 +1,10 @@
-const nickname= "Timmy";
+const nickname = "Timmy";
 const firstname = "Timothy";
-// const displayName = nickname || firstname
 
-
-if ( nickname === firstname) {
-
+if (nickname === firstname) {
     console.log('Good Morning!');
-
-} else if (nickname === firstname) {
-
-    console.log('Good Morning!');
-
 } else {
-
     console.log('Good Morning!');
-
 }
+
+
--- Project IWA7/IWA_7.5 Challenge 4/js/script.js
+++ Project IWA7/IWA_7.5 Challenge 4/js/script.js
@@ -1,27 +1,24 @@
-const leoName = 'Leo'
-const leoSurname = 'Musvaire     '
-const leoBalance = '-9394'
+const leoName = 'Leo';
+const leoSurname = 'Musvaire     ';
+const leoBalance = '-9394';
 
-const sarahName = 'Sarah    '
-const sarahSurname = 'Kleinhans'
-const sarahBalance = '-4582.21000111'
+const sarahName = 'Sarah    ';
+const sarahSurname = 'Kleinhans';
+const sarahBalance = '-4582.21000111';
 
-const divider = '----------------------------------'
+const divider = '----------------------------------';
 
 // Only change below this line
 
-const amountOwedByLeo = parseFloat (Math.abs(leoBalance))
-const amountOwedBySarah = parseFloat(Math.abs(sarahBalance))
-
-const leo = `${leoName} ${leoSurname}(Owed:R ${amountOwedByLeo.toFixed(2)})`
-console.log(leo)
-
-const sarah =`${sarahName}${sarahSurname} (Owed:R ${amountOwedBySarah.toFixed(2)})`
-console.log(sarah)
+const LBalance = parseFloat(Math.abs(leoBalance).toFixed(2)) 
+const SBalance = parseFloat(Math.abs(sarahBalance).toFixed(2)); // Parse as float
+const owed = LBalance + SBalance
 
 
-console.log(divider)
-console.log(totalAmountOwed = "TOTAL AMOUNT OWED:",(amountOwedByLeo + amountOwedBySarah).toFixed(2)) 
-console.log(divider)
+const leo = `${leoName} ${leoSurname} (Owed : R ${LBalance})`;
+const sarah = `${sarahName} ${sarahSurname} (Owed : R ${SBalance})`;
+const total = "Total amount owed: R ";
 
+const result = `${leo}\n${sarah}\n${divider}\n${total}${owed.toFixed(2)}\n${divider}`;
 
+console.log(result);
